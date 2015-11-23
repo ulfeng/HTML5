fedf08ec9021b0c9855869a687283e4f807f3f15
sunshine9 <1104506375@qq.com>
2015-11-23 14:04:52 +0800

    HTML5 Canvas 一张正在眨眼的脸

---
+++ 03 Canvas 基础属性练习/JavaScript.js
@@ -0,0 +1,78 @@
+﻿
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");    // getContext("2d")对象是内建的HTML5对象,拥有多种绘制路径、圆形、矩形、字符及添加对象的方法
+
+// 创建一个红色矩形
+
+//ctx.fillStyle = "#FF0000";
+//ctx.fillRect(0, 0, 150, 75);
+
+// 路径
+
+//ctx.moveTo(0, 0);
+//ctx.lineTo(200, 100);
+//ctx.stroke();
+
+// 圆
+
+//ctx.beginPath();   // 起始一条路径或重置当前路径
+//ctx.arc(95, 50, 45, 0, 2 * Math.PI);  // 圆心坐标 半径 起始角和结束角
+//ctx.stroke();      // 执行
+
+// 文本
+
+//ctx.font = "30px Arial";
+//ctx.strokeText("Hello World", 10, 50);
+
+// 渐变
+
+// Create gradient
+//var grd=ctx.createLinearGradient(0,0,200,0);  // 四个数据为 渐变开始和结束的x,y坐标
+//grd.addColorStop(0, "red");
+//grd.addColorStop(1, "white");
+
+//// Fill with gradient
+//ctx.fillStyle = grd;
+//ctx.fillRect(10,10,150,80)
+
+// 径向圆
+// var grd = ctx.createRadialGradient(75, 50, 5, 90, 60, 100);  // 六个数据为 渐变开始和结束的x,y坐标和半径
+
+// 多个矩形
+//ctx.fillStyle = "rgb(0,0,255)";
+//ctx.fillRect(0, 0, 400, 400);
+//ctx.fillStyle = "rgb(255,0,0)";
+//ctx.fillRect(50, 50, 300, 300);
+//ctx.fillStyle = "rgb(0,255,0)";
+//ctx.fillRect(100, 100, 200, 200);
+//ctx.fillStyle = "#bbb";
+//ctx.fillRect(150, 150, 100, 100);
+
+// 多个矩形 添加透明度
+//ctx.fillStyle = "rgb(0,0,255)";
+//ctx.fillRect(30, 30, 300, 300);
+//ctx.fillStyle = "rgba(255,0,0,0.5)";
+//ctx.fillRect(60, 60, 300, 300);
+//ctx.fillStyle = "rgba(0,255,0,0.25)";
+//ctx.fillRect(90, 90, 300, 300);
+
+// 创建一个放射性渐变
+var grd = ctx.createRadialGradient(300, 300, 0, 300, 300, 300);
+grd.addColorStop("0", "magenta");
+grd.addColorStop("0.25", "blue");
+grd.addColorStop("0.50", "green");
+grd.addColorStop("0.75", "yellow");
+grd.addColorStop("1.0", "red");
+ctx.fillStyle = grd;
+ctx.fillRect(0, 0, 400, 400);
+
+
+
+
+
+
+
+
+
+
+
---
+++ 07 HTML5 Canvas 一张正在眨眼的脸/JavaScript.js
@@ -0,0 +1,113 @@
+﻿
+window.onload = function () {
+    var c = document.getElementById("myCanvasTag");
+    var ctx = c.getContext("2d");
+
+    // draw face
+    ctx.beginPath();
+    ctx.arc(300, 250, 200, 0, 2 * Math.PI, true);
+    ctx.closePath();
+    ctx.stroke();
+
+    // draw left eye
+    ctx.beginPath();
+    ctx.arc(220, 150, 30, 0, 2 * Math.PI, true);
+    ctx.closePath();
+    ctx.fillStyle = "rgb(100,100,225)";
+    ctx.fill();  // fill() 方法填充当前图像颜色
+
+    // draw left iris (虹膜)
+    ctx.beginPath();
+    ctx.arc(220, 150, 10, 0, 2 * Math.PI, true);
+    ctx.closePath();
+    ctx.fillStyle = "rgb(0,0,0)";
+    ctx.fill();
+
+    // draw left eyelid (眼睑)
+    ctx.beginPath();
+    ctx.arc(220, 150, 30, 0, 2 * Math.PI, true);
+    ctx.closePath();
+    ctx.fillStyle = "rgb(200,200,200)";
+    ctx.fill();
+
+    // draw left eyelasher (睫毛)
+    ctx.strokeStyle = "rgb(0,0,0)";
+    lashes(ctx, 198, 170, 193, 185);
+    lashes(ctx, 208, 177, 204, 193);
+    lashes(ctx, 220, 180, 220, 195);
+    lashes(ctx, 232, 177, 236, 193);
+    lashes(ctx, 242, 170, 247, 185);
+    ctx.stroke();
+
+    openEye();
+
+    // draw right eyelashes
+    ctx.strokeStyle = "rgb(0,0,0)";
+    lashes(ctx, 358, 170, 353, 185);
+    lashes(ctx, 368, 177, 363, 193);
+    lashes(ctx, 380, 180, 380, 195);
+    lashes(ctx, 392, 177, 396, 193);
+    lashes(ctx, 402, 170, 407, 185);
+    ctx.stroke();
+
+    // draw nose
+    ctx.beginPath();
+    ctx.arc(300, 250, 20, 0, 2 * Math.PI, true);
+    ctx.closePath();
+    ctx.stroke();
+
+    // draw smile
+    ctx.beginPath();
+    ctx.lineWidth = 10;
+    ctx.moveTo(180, 320);  // 开始点
+    ctx.bezierCurveTo(140, 320, 340, 420, 400, 360);  // 三次贝塞尔曲线的控制点，两个控制点和结束点
+    ctx.closePath();
+    ctx.stroke();
+
+    // draw message at bottom
+    ctx.font = "1em sans-serif";
+    ctx.fillStyle = "rgb(0,0,0)";
+    ctx.fillText("Move the mouse over and off the canvas - the face winks!", 10, 480);
+
+}
+
+function lashes(cntx, x1, y1, x2, y2) {
+    cntx.moveTo(x1, y1);
+    cntx.lineTo(x2, y2);
+}
+
+function closeEye() {
+    // close right eye
+    var c = document.getElementById("myCanvasTag");
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(380, 150, 30, 0, 2 * Math.PI, true);
+    ctx.closePath();
+    ctx.fillStyle = "rgb(200,200,200)";
+    ctx.fill();
+}
+
+function openEye() {
+    // open right eye
+    var c = document.getElementById("myCanvasTag");
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(380, 150, 30, 0, 2 * Math.PI, true);
+    ctx.closePath();
+    ctx.fillStyle = "rgb(100,100,225)";
+    ctx.fill();
+
+    // draw right iris
+    ctx.beginPath();
+    ctx.arc(380, 150, 10, 0, 2 * Math.PI, true);
+    ctx.closePath();
+    ctx.fillStyle = "rgb(0,0,0)";
+    ctx.fill();
+
+    // draw right eyeid
+    ctx.beginPath();
+    ctx.arc(380, 150, 30, 0, 2 * Math.PI, true);
+    ctx.closePath();
+    ctx.fillStyle = "rgb(200,200,200)";
+    ctx.fill();
+}
